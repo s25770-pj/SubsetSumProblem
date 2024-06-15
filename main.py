ca1d0173245318a790ae2ac05c8d776039063862
s25770-pj <s25770@pjwstk.edu.pl>
2024-06-15 12:23:31 +0200

    added tabu and genetic algorithm

--- main.py
+++ main.py
@@ -5,23 +5,72 @@ from subset_sum import *
 if __name__ == "__main__":
     if len(sys.argv) < 4:
         print("Użycie: python main.py <plik_z_liczbami> <algorytm> <szukana_suma> [opcje]")
-        print("Dostępne algorytmy: random, brute_force, hill_climbing_classic")
+        print("Dostępne algorytmy: random, brute_force, hill_climbing_classic, hill_climbing_random, subset_sum_tabu, genetic_algorithm")
         sys.exit(1)
 
     input_file = sys.argv[1]
     algorithm  = sys.argv[2]
     target_sum = int(sys.argv[3])
 
+    tabu_size = 10
+    population_size = 10
+    mutation_rate = 0.01
     max_iterations = 1000
+
+    crossover_method = 'one_point'
+    mutation_method = 'flip_bit'
+    termination_method = 'optimal_solution'
 
     i = 4
     while i < len(sys.argv):
-        if sys.argv[i] == "-iter":
+        if sys.argv[i] == "-population":
+            try:
+                population_size = int(sys.argv[i + 1])
+                i += 2
+            except (IndexError, ValueError):
+                print("Nieprawidłowy argument dla -population")
+                sys.exit(1)
+        elif sys.argv[i] == "-mutation_rate":
+            try:
+                mutation_rate = float(sys.argv[i + 1])
+                i += 2
+            except (IndexError, ValueError):
+                print("Nieprawidłowy argument dla -mutation_rate")
+                sys.exit(1)
+        elif sys.argv[i] == "-iter":
             try:
                 max_iterations = int(sys.argv[i + 1])
                 i += 2
             except (IndexError, ValueError):
                 print("Nieprawidłowy argument dla -iter")
+                sys.exit(1)
+        elif sys.argv[i] == "-tabu_size":
+            try:
+                tabu_size = int(sys.argv[i + 1])
+                i += 2
+            except (IndexError, ValueError):
+                print("Nieprawidłowy argument dla -tabu_size")
+                sys.exit(1)
+        elif sys.argv[i] == "-crossover_method":
+            try:
+                crossover_method = sys.argv[i + 1]
+                i += 2
+            except IndexError:
+                print("Nieprawidłowy argument dla -crossover_method")
+                sys.exit(1)
+        elif sys.argv[i] == "-mutation_method":
+            try:
+                mutation_method = sys.argv[i + 1]
+                i += 2
+            except IndexError:
+                print("Nieprawidłowy argument dla -mutation_method")
+                sys.exit(1)
+        elif sys.argv[i] == "-termination_method":
+            try:
+                termination_method = sys.argv[i + 1]
+                i += 2
+            except IndexError:
+                print("Nieprawidłowy argument dla -termination_method")
                 sys.exit(1)
         else:
             print("Nieznana opcja:", sys.argv[i])
@@ -36,12 +85,19 @@ if __name__ == "__main__":
         solution = brute_force(numbers, target_sum)
     elif algorithm == "hill_climbing_classic":
         solution = hill_climbing_classic(numbers, target_sum, max_iterations)
+    elif algorithm == "hill_climbing_random":
+        solution = hill_climbing_random(numbers, target_sum, max_iterations)
+    elif algorithm == "subset_sum_tabu":
+        solution = subset_sum_tabu(numbers, target_sum, tabu_size, max_iterations)
+    elif algorithm == "genetic_algorithm":
+        solution = genetic_algorithm(numbers, target_sum, population_size, max_iterations, crossover_method, mutation_method, termination_method)
     else:
         print("Nieznany algorytm:", algorithm)
         sys.exit(1)
 
     if solution:
         print("Znalezione rozwiązanie:", solution)
-        print("Suma rozwiązania", sum(solution[0]))
+        if type(solution) == list:
+            print("Suma rozwiązania", sum(solution[0]))
     else:
         print("Nie znaleziono rozwiązania dla szukanej sumy", target_sum)
--- subset_sum.py
+++ subset_sum.py
@@ -1,5 +1,6 @@
 import random
 import itertools
+from collections import deque
 
 #########################################################################################################
 # General utilities
@@ -18,6 +19,12 @@ def generate_neighborhood(S, current_solution):
         neighbor_solution[i] = not neighbor_solution[i]
         neighbors.append(neighbor_solution)
     return neighbors
+
+def generate_random_neighbor(S, current_solution):
+    neighbor_solution = current_solution[:]
+    index = random.randint(0, len(S) - 1)
+    neighbor_solution[index] = not neighbor_solution[index]
+    return neighbor_solution
 
 def swap_bool_with_numbers(S, current_solution):
     return [S[i] for i in range(len(S)) if current_solution[i]]
@@ -50,23 +57,165 @@ def hill_climbing_classic(S, T, max_iterations):
     
     for iteration in range(max_iterations):
         print(f'Iteration: {iteration + 1}')
+
         neighbors = generate_neighborhood(S, current_solution)
-        
         best_neighbor = None
-        best_value = current_value
+        best_neighbor_value = float('inf')
+
         for neighbor in neighbors:
             neighbor_value = objective_function(S, T, neighbor)
-            if neighbor_value < best_value:
-                best_value = neighbor_value
+            if neighbor_value < best_neighbor_value:
                 best_neighbor = neighbor
-        
-        if best_neighbor is None:
+                best_neighbor_value = neighbor_value
+
+        if best_neighbor is not None and best_neighbor_value < current_value:
+            current_solution = best_neighbor
+            current_value = best_neighbor_value
+            print(f'New best value: {current_value}')
+        else:
             break
+
+    return swap_bool_with_numbers(S, current_solution), current_value
+
+#########################################################################################################
+# Algorytmy wspinaczkowy losowy
+#########################################################################################################
+def hill_climbing_random(S, T, max_iterations):
+    current_solution = generate_random_solution(S)
+    current_value = objective_function(S, T, current_solution)
+    
+    for iteration in range(max_iterations):
+        print(f'Iteration: {iteration + 1}')
+
+        if current_value == 0:
+            return swap_bool_with_numbers(S, current_solution), current_value
+
+        neighbor = generate_random_neighbor(S, current_solution)
+        neighbor_value = objective_function(S, T, neighbor)
+        if neighbor_value < current_value:
+            current_solution = neighbor
+            current_value = neighbor_value
+    
+    return swap_bool_with_numbers(S, current_solution), current_value
+
+#########################################################################################################
+# Algorytm tabu
+#########################################################################################################
+def subset_sum_tabu(S, T, tabu_size, max_iterations):
+    tabu_list = deque(maxlen=tabu_size)
+
+    current_solution = generate_random_solution(S)
+    current_value  = objective_function(S, T, current_solution)
+
+    best_solution = current_solution[:]
+    best_value = current_value
+    
+    tabu_list.append(current_solution)
+
+    for iteration in range(max_iterations):
+        print(f'Iteration: {iteration+1}')
+
+        neighbors = generate_neighborhood(S, current_solution)
+        best_neighbor = None
+        best_neighbor_value = float('inf')
+
+        for neighbor in neighbors:
+            if neighbor not in tabu_list:
+                neighbor_value = objective_function(S, T, neighbor)
+                if neighbor_value < best_neighbor_value:
+                    best_neighbor = neighbor
+                    best_neighbor_value = neighbor_value
         
-        current_solution = best_neighbor
-        current_value = best_value
+        if best_neighbor is not None:
+            current_solution = best_neighbor
+            current_value = best_neighbor_value
+
+            if current_value < best_value:
+                best_solution = current_solution[:]
+                best_value = current_value
+
+            tabu_list.append(current_solution[:])
+        else:
+            if len(tabu_list) > 1:
+                tabu_list.pop()
+                current_solution = tabu_list[-1]
+                current_value = objective_function(S, T, current_solution)
+
+        if best_value == 0:
+            break
+
+    return swap_bool_with_numbers(S, best_solution), best_value
+
+#########################################################################################################
+# Algorytm genetyczny
+#########################################################################################################
+def crossover_one_point(parent1, parent2):
+    point = random.randint(1, len(parent1) - 1)
+    child1 = parent1[:point] + parent2[point:]
+    child2 = parent2[:point] + parent1[point:]
+    return child1, child2
+
+def crossover_two_point(parent1, parent2):
+    point1 = random.randint(1, len(parent1) - 2)
+    point2 = random.randint(point1 + 1, len(parent1) - 1)
+    child1 = parent1[:point1] + parent2[point1:point2] + parent1[point2:]
+    child2 = parent2[:point1] + parent1[point1:point2] + parent2[point2:]
+    return child1, child2
+
+def mutation_flip_bit(solution):
+    point = random.randint(0, len(solution) - 1)
+    solution[point] = not solution[point]
+    return solution
+
+def mutation_swap_bits(solution):
+    point1, point2 = random.sample(range(len(solution)), 2)
+    solution[point1], solution[point2] = solution[point2], solution[point1]
+    return solution
+
+def select_parents(population, fitness, num_parents):
+    parents = random.choices(population, weights=fitness, k=num_parents)
+    return parents
+
+def genetic_algorithm(S, T, pop_size, max_iterations, crossover_method, mutation_method, termination_condition):
+    population = [generate_random_solution(S) for _ in range(pop_size)]
+    fitness = [1 / (1 + objective_function(S, T, sol)) for sol in population]
     
-    best_subset = swap_bool_with_numbers(S, current_solution)
-    best_value = objective_function(S, T, current_solution)
+    for generation in range(max_iterations):
+        print(f'Generation: {generation + 1}')
+        
+        parents = select_parents(population, fitness, pop_size)
+        
+        next_population = []
+        for i in range(0, pop_size, 2):
+            parent1 = parents[i]
+            parent2 = parents[i + 1] if i + 1 < pop_size else parents[0]
+            
+            if crossover_method == 'one_point':
+                child1, child2 = crossover_one_point(parent1, parent2)
+            elif crossover_method == 'two_point':
+                child1, child2 = crossover_two_point(parent1, parent2)
+            else:
+                raise ValueError("Invalid crossover method specified.")
+            
+            if mutation_method == 'flip_bit':
+                child1 = mutation_flip_bit(child1)
+                child2 = mutation_flip_bit(child2)
+            elif mutation_method == 'swap_bits':
+                child1 = mutation_swap_bits(child1)
+                child2 = mutation_swap_bits(child2)
+            else:
+                raise ValueError("Invalid mutation method specified.")
+            
+            next_population.extend([child1, child2])
+        
+        population = next_population
+        fitness = [1 / (1 + objective_function(S, T, sol)) for sol in population]
     
-    return best_subset, T - best_value
+        if termination_condition == 'fixed_generations':
+            if generation + 1 >= max_iterations:
+                break
+        elif termination_condition == 'optimal_solution':
+            if 0 in [objective_function(S, T, sol) for sol in population]:
+                best_solution = min(population, key=lambda sol: objective_function(S, T, sol))
+                best_value = objective_function(S, T, best_solution)
+                return swap_bool_with_numbers(S, best_solution), best_value
